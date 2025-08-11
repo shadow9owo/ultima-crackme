bb77899b0656d12f2276985251d30e9570eba232
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2025-08-11 19:15:21 +0200

    Delete wrapper.h

--- wrapper.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-extern unsigned long unixtimestamp();
-extern char* rotN(char* input, int diff);
-extern int wraparound(int input,int min,int max);
-extern unsigned short fl_checksum(char* input);
